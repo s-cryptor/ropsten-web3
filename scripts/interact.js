a9254fac20cee39339338cd249986c89f5f765f9
s-cryptor <grisga2001@gmail.com>
2022-04-27 17:59:23 +0300

    add update tx

--- scripts/interact.js
+++ scripts/interact.js
@@ -16,5 +16,12 @@ const ropstenContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, sign
 async function main() {
   const message = await ropstenContract.message()
   console.log("The message is: " + message)
+
+  console.log("Updating the message...")
+  const tx = await ropstenContract.update("New Ropsten (v2).")
+  await tx.wait()
+
+  const newMessage = await ropstenContract.message()
+  console.log("The new message is: " + newMessage)
 }
 main()
