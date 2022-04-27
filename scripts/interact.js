591fdcbc48394c2b787a7e31dcb4c50a03fa837e
s-cryptor <grisga2001@gmail.com>
2022-04-27 17:54:08 +0300

    add interact with blockchain

---
+++ scripts/interact.js
@@ -0,0 +1,20 @@
+const API_KEY = process.env.API_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS
+
+const contract = require("../artifacts/contracts/Ropsten.sol/Ropsten.json")
+
+// Provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(network="ropsten", API_KEY)
+
+// Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider)
+
+// Contract
+const ropstenContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer)
+
+async function main() {
+  const message = await ropstenContract.message()
+  console.log("The message is: " + message)
+}
+main()
