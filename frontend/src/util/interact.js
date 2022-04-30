63880871be743a9b183b59aa84dc3c3337114c89
s-cryptor <grisga2001@gmail.com>
2022-04-30 15:12:57 +0300

    add frontend src template

---
+++ frontend/src/util/interact.js
@@ -0,0 +1,9 @@
+//export const helloWorldContract;
+
+export const loadCurrentMessage = async () => {}
+
+export const connectWallet = async () => {}
+
+export const getCurrentWalletConnected = async () => {}
+
+export const updateMessage = async (address, message) => {}
